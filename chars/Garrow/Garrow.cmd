4385ab76263a66b9ce8d686824741f8a84176c21
bentaecool <tdsbentaecool@gmail.com>
2026-07-26 16:05:46 -0400

    colours

--- chars/Garrow/Garrow.cmd
+++ chars/Garrow/Garrow.cmd
@@ -154,24 +154,24 @@ command.buffer.time = 1
 ;
 [Command]
 name = "TripleKFPalm"
-command = ~D, DF, F, D, DF, x
+command = ~D, DF, F, D, DF, F, x
 time = 20
-buffer.time=3
+
 [Command]
 name = "TripleKFPalm"   ;Same name as above
-command = ~D, DF, F, D, DF, y
+command = ~D, DF, F, D, DF, F, y
 time = 20
-buffer.time=3
+
 [Command]
 name = "SmashKFUpper"
-command = ~D, DB, B, D, DB, x;~F, D, DF, F, D, DF, x
+command = ~D, DB, B, D, DB, B, x;~F, D, DF, F, D, DF, x
 time = 20
-buffer.time=3
+
 [Command]
 name = "SmashKFUpper"   ;Same name as above
-command = ~D, DB, B, D, DB, y;~F, D, DF, F, D, DF, y
+command = ~D, DB, B, D, DB, B, y;~F, D, DF, F, D, DF, y
 time = 20
-buffer.time=3
+
 ;-| Special Motions |------------------------------------------------------
 [Command]
 name = "blocking"
@@ -186,63 +186,63 @@ time = 3
 [Command]
 name = "upper_x"
 command = ~F, D, DF, x
-buffer.time=3
+
 [Command]
 name = "upper_y"
 command = ~F, D, DF, y
-buffer.time=3
+
 [Command]
 name = "upper_xy"
 command = ~F, D, DF, x+y
-buffer.time=3
+
 [Command]
 name = "QCF_x"
 command = ~D, DF, F, x
-buffer.time=3
+
 [Command]
 name = "QCF_y"
 command = ~D, DF, F, y
-buffer.time=3
+
 [Command]
 name = "QCF_xy"
 command = ~D, DF, F, x+y
-buffer.time=3
+
 [Command]
 name = "QCB_x"
 command = ~D, DB, B, x
-buffer.time=3
+
 [Command]
 name = "QCB_y"
 command = ~D, DB, B, y
-buffer.time=3
+
 [Command]
 name = "QCB_xy"
 command = ~D, DB, B, x+y
-buffer.time=3
+
 [Command]
 name = "QCF_a"
 command = ~D, DF, F, a
-buffer.time=3
+
 [Command]
 name = "QCF_b"
 command = ~D, DF, F, b
-buffer.time=3
+
 [Command]
 name = "QCF_ab"
 command = ~D, DF, F, a+b
-buffer.time=3
+
 [Command]
 name = "FF_ab"
 command = F, F, a+b
-buffer.time=3
+
 [Command]
 name = "FF_a"
 command = F, F, a
-buffer.time=3
+
 [Command]
 name = "FF_b"
 command = F, F, b
-buffer.time=3
+
 ;-| Double Tap |-----------------------------------------------------------
 [Command]
 name = "FF"     ;Required (do not remove)
@@ -275,32 +275,32 @@ time = 1
 [Command]
 name = "a"
 command = a
-time = 3
+time = 1
 
 [Command]
 name = "b"
 command = b
-time = 3
+time = 1
 
 [Command]
 name = "c"
 command = c
-time = 3
+time = 1
 
 [Command]
 name = "x"
 command = x
-time = 3
+time = 1
 
 [Command]
 name = "y"
 command = y
-time = 3
+time = 1
 
 [Command]
 name = "z"
 command = z
-time = 3
+time = 1
 
 [Command]
 name = "start"
@@ -328,3 +328,448 @@ name = "holddown";Required (do not remove)
 command = /$D
 time = 1
 
+;---------------------------------------------------------------------------
+; 2. State entry
+; --------------
+; This is where you define what commands bring you to what states.
+;
+; Each state entry block looks like:
+;   [State -1, Label]           ;Change Label to any name you want to use to
+;                               ;identify the state with.
+;   type = ChangeState          ;Don't change this
+;   value = new_state_number
+;   trigger1 = command = command_name
+;   . . .  (any additional triggers)
+;
+; - new_state_number is the number of the state to change to
+; - command_name is the name of the command (from the section above)
+; - Useful triggers to know:
+;   - statetype
+;       S, C or A : current state-type of player (stand, crouch, air)
+;   - ctrl
+;       0 or 1 : 1 if player has control. Unless "interrupting" another
+;                move, you'll want ctrl = 1
+;   - stateno
+;       number of state player is in - useful for "move interrupts"
+;   - movecontact
+;       0 or 1 : 1 if player's last attack touched the opponent
+;                useful for "move interrupts"
+;
+; Note: The order of state entry is important.
+;   State entry with a certain command must come before another state
+;   entry with a command that is the subset of the first.
+;   For example, command "fwd_a" must be listed before "a", and
+;   "fwd_ab" should come before both of the others.
+;
+; For reference on triggers, see CNS documentation.
+;
+; Just for your information (skip if you're not interested):
+; This part is an extension of the CNS. "State -1" is a special state
+; that is executed once every game-tick, regardless of what other state
+; you are in.
+
+
+; Don't remove the following line. It's required by the CMD standard.
+[Statedef -1]
+
+;===========================================================================
+;---------------------------------------------------------------------------
+;Smash Kung Fu Upper (uses one super bar)
+[State -1, Smash Kung Fu Upper]
+type = ChangeState
+value = 3050
+triggerall = command = "SmashKFUpper"
+triggerall = power >= 1000
+triggerall = statetype != A
+trigger1 = ctrl
+trigger2 = hitdefattr = SC, NA, SA, HA
+trigger2 = stateno != [3050,3100)
+trigger2 = movecontact
+trigger3 = stateno = 1310 || stateno = 1330 ;From blocking
+
+;---------------------------------------------------------------------------
+;Triple Kung Fu Palm (uses one super bar)
+[State -1, Triple Kung Fu Palm]
+type = ChangeState
+value = 3000
+triggerall = command = "TripleKFPalm"
+triggerall = power >= 1000
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = statetype != A
+trigger2 = hitdefattr = SC, NA, SA, HA
+trigger2 = stateno != [3000,3050)
+trigger2 = movecontact
+trigger3 = stateno = 1310 || stateno = 1330 ;From blocking
+
+
+;===========================================================================
+;This is not a move, but it sets up var(1) to be 1 if conditions are right
+;for a combo into a special move (used below).
+;Since a lot of special moves rely on the same conditions, this reduces
+;redundant logic.
+[State -1, Combo condition Reset]
+type = VarSet
+trigger1 = 1
+var(1) = 0
+
+[State -1, Combo condition Check]
+type = VarSet
+trigger1 = statetype != A
+trigger1 = ctrl
+trigger2 = (stateno = [200,299]) || (stateno = [400,499])
+trigger2 = stateno != 440 ;Except for sweep kick
+trigger2 = movecontact
+trigger3 = stateno = 1310 || stateno = 1330 ;From blocking
+var(1) = 1
+
+;---------------------------------------------------------------------------
+;Fast Kung Fu Knee (1/3 super bar)
+[State -1, Fast Kung Fu Knee]
+type = ChangeState
+value = 1070
+triggerall = command = "FF_ab"
+triggerall = power >= 330
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Light Kung Fu Knee
+[State -1, Light Kung Fu Knee]
+type = ChangeState
+value = 1050
+triggerall = command = "FF_a"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Strong Kung Fu Knee
+[State -1, Strong Kung Fu Knee]
+type = ChangeState
+value = 1060
+triggerall = command = "FF_b"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Fast Kung Fu Palm (1/3 super bar)
+[State -1, Fast Kung Fu Palm]
+type = ChangeState
+value = 1020
+triggerall = command = "QCF_xy"
+triggerall = power >= 330
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Light Kung Fu Palm
+[State -1, Light Kung Fu Palm]
+type = ChangeState
+value = 1000
+triggerall = command = "QCF_x"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Strong Kung Fu Palm
+[State -1, Strong Kung Fu Palm]
+type = ChangeState
+value = 1010
+triggerall = command = "QCF_y"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Fast Kung Fu Upper (1/3 super bar)
+[State -1, Fast Kung Fu Upper]
+type = ChangeState
+value = 1120
+triggerall = command = "upper_xy"
+triggerall = power >= 330
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Light Kung Fu Upper
+[State -1, Light Kung Fu Upper]
+type = ChangeState
+value = 1100
+triggerall = command = "upper_x"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Strong Kung Fu Upper
+[State -1, Strong Kung Fu Upper]
+type = ChangeState
+value = 1110
+triggerall = command = "upper_y"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Fast Kung Fu Blow (1/3 super bar)
+[State -1, Fast Kung Fu Blow]
+type = ChangeState
+value = 1220
+triggerall = command = "QCB_xy"
+triggerall = power >= 330
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Light Kung Fu Blow
+[State -1, Light Kung Fu Blow]
+type = ChangeState
+value = 1200
+triggerall = command = "QCB_x"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Strong Kung Fu Blow
+[State -1, Strong Kung Fu Blow]
+type = ChangeState
+value = 1210
+triggerall = command = "QCB_y"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;High Kung Fu Blocking (High)
+[State -1, High Kung Fu Blocking High]
+type = ChangeState
+value = 1300
+triggerall = command = "blocking"
+triggerall = command != "holddown"
+trigger1 = ctrl
+trigger1 = statetype != A
+trigger2 = stateno = 1310 || stateno = 1330
+trigger2 = time > 0
+
+;---------------------------------------------------------------------------
+;High Kung Fu Blocking (Low)
+[State -1, High Kung Fu Blocking Low]
+type = ChangeState
+value = 1320
+triggerall = command = "blocking"
+triggerall = command = "holddown"
+trigger1 = ctrl
+trigger1 = statetype != A
+trigger2 = stateno = 1310 || stateno = 1330
+trigger2 = time > 0
+
+;---------------------------------------------------------------------------
+;High Kung Fu Blocking (Air)
+[State -1, High Kung Fu Blocking Low]
+type = ChangeState
+value = 1340
+triggerall = command = "blocking"
+triggerall = command != "holdup"
+triggerall = command != "holddown"
+trigger1 = ctrl
+trigger1 = statetype = A
+trigger2 = stateno = 1350
+trigger2 = time > 0
+
+;---------------------------------------------------------------------------
+;Far Kung Fu Zankou
+[State -1, Far Kung Fu Zankou]
+type = ChangeState
+value = 1420
+triggerall = command = "QCF_ab"
+triggerall = power >= 330
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Light Kung Fu Zankou
+[State -1, Light Kung Fu Zankou]
+type = ChangeState
+value = 1400
+triggerall = command = "QCF_a"
+trigger1 = var(1) ;Use combo condition (above)
+
+;---------------------------------------------------------------------------
+;Strong Kung Fu Zankou
+[State -1, Strong Kung Fu Zankou]
+type = ChangeState
+value = 1410
+triggerall = command = "QCF_b"
+trigger1 = var(1) ;Use combo condition (above)
+
+;===========================================================================
+;---------------------------------------------------------------------------
+;Run Fwd
+[State -1, Run Fwd]
+type = ChangeState
+value = 100
+trigger1 = command = "FF"
+trigger1 = statetype = S
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+;Run Back
+[State -1, Run Back]
+type = ChangeState
+value = 105
+trigger1 = command = "BB"
+trigger1 = statetype = S
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+;Kung Fu Throw
+[State -1, Kung Fu Throw]
+type = ChangeState
+value = 800
+triggerall = command = "y"
+triggerall = statetype = S
+triggerall = ctrl
+triggerall = stateno != 100
+trigger1 = command = "holdfwd"
+trigger1 = p2bodydist X < 3
+trigger1 = (p2statetype = S) || (p2statetype = C)
+trigger1 = p2movetype != H
+trigger2 = command = "holdback"
+trigger2 = p2bodydist X < 5
+trigger2 = (p2statetype = S) || (p2statetype = C)
+trigger2 = p2movetype != H
+
+
+
+;===========================================================================
+;---------------------------------------------------------------------------
+;Stand Light Punch
+[State -1, Stand Light Punch]
+type = ChangeState
+value = 200
+triggerall = command = "x"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = stateno = 200
+trigger2 = time > 6
+
+;---------------------------------------------------------------------------
+;Stand Strong Punch
+[State -1, Stand Strong Punch]
+type = ChangeState
+value = 210
+triggerall = command = "y"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = (stateno = 200) && time > 5
+trigger3 = (stateno = 230) && time > 6
+
+;---------------------------------------------------------------------------
+;Stand Light Kick
+[State -1, Stand Light Kick]
+type = ChangeState
+value = 230
+triggerall = command = "a"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = (stateno = 200) && time > 7
+trigger3 = (stateno = 230) && time > 9
+
+;---------------------------------------------------------------------------
+;Standing Strong Kick
+[State -1, Standing Strong Kick]
+type = ChangeState
+value = 240
+triggerall = command = "b"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+trigger1 = ctrl
+trigger2 = (stateno = 200) && time > 5
+trigger3 = (stateno = 230) && time > 6
+
+;---------------------------------------------------------------------------
+;Taunt
+[State -1, Taunt]
+type = ChangeState
+value = 195
+triggerall = command = "start"
+trigger1 = statetype != A
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+;Crouching Light Punch
+[State -1, Crouching Light Punch]
+type = ChangeState
+value = 400
+triggerall = command = "x"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+trigger1 = ctrl
+
+;---------------------------------------------------------------------------
+;Crouching Strong Punch
+[State -1, Crouching Strong Punch]
+type = ChangeState
+value = 410
+triggerall = command = "y"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+trigger1 = ctrl
+trigger2 = (stateno = 400) || (stateno = 430)
+trigger2 = (time > 9) || (movecontact && time > 5)
+
+;---------------------------------------------------------------------------
+;Crouching Light Kick
+[State -1, Crouching Light Kick]
+type = ChangeState
+value = 430
+triggerall = command = "a"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+trigger1 = ctrl
+trigger2 = (stateno = 400) || (stateno = 430)
+trigger2 = (time > 9) || (movecontact && time > 5)
+
+;---------------------------------------------------------------------------
+;Crouching Strong Kick
+[State -1, Crouching Strong Kick]
+type = ChangeState
+value = 440
+triggerall = command = "b"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+trigger1 = ctrl
+trigger2 = (stateno = 400) || (stateno = 430)
+trigger2 = (time > 9) || (movecontact && time > 5)
+
+;---------------------------------------------------------------------------
+;Jump Light Punch
+[State -1, Jump Light Punch]
+type = ChangeState
+value = 600
+triggerall = command = "x"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = stateno = 600
+trigger2 = statetime >= 7
+trigger3 = stateno = 1350 ;Air blocking
+
+;---------------------------------------------------------------------------
+;Jump Strong Punch
+[State -1, Jump Strong Punch]
+type = ChangeState
+value = 610
+triggerall = command = "y"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = stateno = 600 || stateno = 630 ;jump_x or jump_a
+trigger2 = movecontact
+trigger3 = stateno = 1350 ;Air blocking
+
+;---------------------------------------------------------------------------
+;Jump Light Kick
+[State -1, Jump Light Kick]
+type = ChangeState
+value = 630
+triggerall = command = "a"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = stateno = 1350 ;Air blocking
+
+;---------------------------------------------------------------------------
+;Jump Strong Kick
+[State -1, Jump Strong Kick]
+type = ChangeState
+value = 640
+triggerall = command = "b"
+trigger1 = statetype = A
+trigger1 = ctrl
+trigger2 = stateno = 600 || stateno = 630 ;jump_x or jump_a
+trigger2 = movecontact
+trigger3 = stateno = 1350 ;Air blocking
